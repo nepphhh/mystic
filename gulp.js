d51865be4a335127479992714ba2064e59e5a609
Neph <nephdev@gmail.com>
2018-12-18 13:36:48 -0500

    Flushing more shit

--- gulp.js
+++ gulp.js
@@ -1,22 +1,36 @@
-import { task } from 'gulp';
-import { exec } from 'child_process';
+/*jshint node: true, esversion: 6*/
+/*global goog, Map, let */
 
-gulp = require('gulp');
-newer = require('gulp-newer');
-imagemin = require('gulp-imagemin');
-htmlclean = require('gulp-htmlclean');
+"use strict";
+
+const
+	child_process = require('child_process'),
+	gulp = require('gulp'),
+	newer = require('gulp-newer'),
+	imagemin = require('gulp-imagemin'),
+	htmlclean = require('gulp-htmlclean'),
+	concat = require('gulp-concat'),
+	deporder = require('gulp-deporder'),
+	stripdebug = require('gulp-strip-debug'),
+	uglify = require('gulp-uglify');
 
 // Folders
-folder = {
+const folder = {
 	src: 'src/',
 	build: 'build/'
 };
 
-// Rust server
-var buildCommand = "cargo run";
+// Build rust server
+const buildCommand = "cargo run";
+gulp.task('rust', () => {
+	child_process.exec(buildCommand, (error, stdout, stderr) => {
+		console.log(stdout);
+		console.log(stderr);
+	});
+});
 
 // Compress images
-task('images', () => {
+gulp.task('images', () => {
 	var out = folder.build + 'images/';
 	return gulp.src(folder.src + 'images/**/*')
 	  .pipe(newer(out))
@@ -24,33 +38,44 @@ task('images', () => {
 	  .pipe(gulp.dest(out));
 });  
 
-// Build rust server
-task('build', () => {
-	exec(buildCommand, (error, stdout, stderr) => {
-		console.log(stdout);
-		console.log(stderr);
-	});
-});
-
-task('watch', () => {
-	gulp.watch('src/*.cpp', ['build']);
-	gulp.watch('include/*.h', ['build']);
-});
-
 // HTML processing
-task('html', ['images'], function() {
-	var
-	  out = folder.build + 'html/',
-	  page = gulp.src(folder.src + 'html/**/*')
-		.pipe(newer(out));
-  
-	// minify production code
-	if (!devBuild) {
-	  page = page.pipe(htmlclean());
-	}
-  
-	return page.pipe(gulp.dest(out));
-  });
-  
+gulp.task('html', gulp.series('images', () => {
+	var out = folder.build + 'html/',
+		page = gulp.src(folder.src + 'html/**/*')
+			.pipe(newer(out))
+			.pipe(htmlclean());
 
-task('default', ['build', 'watch'], () => {} );
+	return page.pipe(gulp.dest(out));
+}));
+  
+// JavaScript processing
+gulp.task('js', () => {
+	var jsbuild = gulp.src(folder.src + 'js/**/*')
+		.pipe(deporder())
+		.pipe(concat('main.js'))
+		.pipe(stripdebug())
+		.pipe(uglify());
+
+	return jsbuild.pipe(gulp.dest(folder.build + 'js/'));  
+});
+
+// Watch everything
+gulp.task('watch', () => {
+  
+	// rust changes
+	gulp.watch(folder.src + 'rust/**/*', ['rust']);
+
+	// image changes
+	gulp.watch(folder.src + 'images/**/*', ['images']);
+  
+	// html changes
+	gulp.watch(folder.src + 'html/**/*', ['html']);
+  
+	// javascript changes
+	gulp.watch(folder.src + 'js/**/*', ['js']);
+  
+});  
+
+gulp.task('run', gulp.parallel('rust', 'html', 'js'));
+  
+gulp.task('default', gulp.series('run', 'watch'));
